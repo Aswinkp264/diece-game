c93a66c319dcbec5ec82e3e08ec1583094910798
aswinkp <kpaswin264@gmail.com>
2024-09-09 11:20:49 +0530

    hideen diece

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ dieceEl1.classList.add("hidden");
 const score = [0, 0];
 let activePlayer = 0;
 let currentScore = 0;
+let playing = true;
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -29,51 +30,56 @@ const switchPlayer = function () {
 //rolling diece functionality
 
 btnRoll.addEventListener("click", function () {
-  // genarate random diece roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // console.log(dice);
+  if (playing) {
+    // genarate random diece roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // console.log(dice);
 
-  // score storing
+    // score storing
 
-  dieceEl1.classList.remove("hidden");
-  dieceEl1.src = `dice-${dice}.png`;
+    dieceEl1.classList.remove("hidden");
+    dieceEl1.src = `dice-${dice}.png`;
 
-  //switching when get 1
+    //switching when get 1
 
-  if (dice !== 1) {
-    // add to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    if (dice !== 1) {
+      // add to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
 
-    // current0El.textContent = currentScore; //change upcoming session
-  } else {
-    // switch to next player
-    switchPlayer();
+      // current0El.textContent = currentScore; //change upcoming session
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
   }
 });
 btnHold.addEventListener("click", function () {
-  // console.log("hold button");
+  if (playing) {
+    // console.log("hold button");
 
-  //adding cuurent score of active player
+    //adding cuurent score of active player
 
-  score[activePlayer] += currentScore;
-  // score[1] = score[1]+currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    score[activePlayer];
+    score[activePlayer] += currentScore;
+    // score[1] = score[1]+currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
 
-  /// check score to minimum hunderd
+    /// check score to minimum hunderd
 
-  if (score[activePlayer] >= 20) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add("player--winner");
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove(".player--active");
-  } 
-  else {
-    // switch player
-    switchPlayer();
+    if (score[activePlayer] >= 20) {
+      playing = false;
+      dieceEl1.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(".player--active");
+    } else {
+      // switch player
+      switchPlayer();
+    }
   }
 });
